eb8c09f74451da410d6144c15527f1d9a802bef5
Vadim Kiselev <vadim8kiselev@gmail.com>
2015-04-15 21:17:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,18 +17,19 @@
             var months = ["", " января ", " февраля ", " марта ", " апреля ", " мая ", " июня ", " июля ", " августа ", " сентября ", " октября ", " ноября ", " декабря "];
             var days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', '']
             var time = new Date();
-            var lmonth = months[time.getMonth() + 1];
+            var month = months[time.getMonth() + 1];
             var date = time.getDate();
             var year = time.getFullYear();
             var day = time.getDay() + 1;
-            var h = time.getHours();
+            var hour = time.getHours();
             var tod = days[time.getDay()];
+            
             var welcome;
-            if (h >= 0 && h < 6) welcome = "Доброй ночи!";
-            if (h >= 6 && h < 12) welcome = "Доброе утро!";
-            if (h >= 12 && h < 18) welcome = "Добрый день!";
-            if (h >= 18 && h <= 23) welcome = "Добрый вечер!";
-            document.getElementById("hello").innerHTML = welcome + " Сегодня " +  date + lmonth + year + " года"+" ("+ tod + ")";
+            if (hour >= 0 && hour < 6) welcome = "Доброй ночи!";
+            if (hour >= 6 && hour < 12) welcome = "Доброе утро!";
+            if (hour >= 12 && hour < 18) welcome = "Добрый день!";
+            if (hour >= 18 && hour <= 23) welcome = "Добрый вечер!";
+            document.getElementById("hello").innerHTML = welcome + " Сегодня " +  date + month + year + " года"+" ("+ tod + ")";
         }
     </script>
     <script type="text/javascript">
