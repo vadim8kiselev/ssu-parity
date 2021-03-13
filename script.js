32b6cc65009ff8ed75760819e5c33c4c41f5e5d8
Anastasiia Ovcharova <anastasiaov1998@mail.ru>
2021-03-14 01:23:48 +0400

    Refactoring

--- script.js
+++ script.js
@@ -8,26 +8,26 @@ $(document).ready(function (){
     });
 })
 
-var dayFirstOfSeptember = (new Date(new Date().getFullYear(), 8, 1)).getDay();
-var dateFirstMondayInSeptember = (dayFirstOfSeptember == 2) ? 7 : (9 - dayFirstOfSeptember) % 7;
+let dayFirstOfSeptember = (new Date(new Date().getFullYear(), 8, 1)).getDay();
+let dateFirstMondayInSeptember = (dayFirstOfSeptember === 2) ? 7 : (9 - dayFirstOfSeptember) % 7;
 
-var dayFirstOfJanuary = (new Date(new Date().getFullYear(), 0, 1)).getDay();
-var dateFirstMondayInJanuary = (dayFirstOfJanuary == 2) ? 7 : (9 - dayFirstOfJanuary) % 7;
+let dayFirstOfJanuary = (new Date(new Date().getFullYear(), 0, 1)).getDay();
+let dateFirstMondayInJanuary = (dayFirstOfJanuary === 2) ? 7 : (9 - dayFirstOfJanuary) % 7;
 
-var currentDate = new Date().getDate();
-var currentMonth = new Date().getMonth();
+let currentDate = new Date().getDate();
+let currentMonth = new Date().getMonth();
 
-var countFullWeeksBeforeNY = Math.floor((new Date().getTime() - new Date(new Date().getFullYear(), 8, dateFirstMondayInSeptember).getTime()) / 86400000 / 7);
-var countFullWeeksAfterNY = Math.floor((new Date().getTime() - new Date(new Date().getFullYear(), 0, dateFirstMondayInJanuary).getTime()) / 86400000 / 7);
+let countFullWeeksBeforeNY = Math.floor((new Date().getTime() - new Date(new Date().getFullYear(), 8, dateFirstMondayInSeptember).getTime()) / 86400000 / 7);
+let countFullWeeksAfterNY = Math.floor((new Date().getTime() - new Date(new Date().getFullYear(), 0, dateFirstMondayInJanuary).getTime()) / 86400000 / 7);
 
-var bars = {
+let bars = {
     summer: false,
     swapped: false,
 
-    style: 'blue.css',
+    style: 'red.css',
 
     swap: function () {
-        this.style = 'red.css';
+        this.style = 'blue.css';
         this.swapped = true
     },
 
@@ -37,9 +37,9 @@ var bars = {
     },
 
     setStyle: function () {
-        var link = document.createElement('link');
-        var attrRel = document.createAttribute('rel');
-        var attrHref = document.createAttribute('href');
+        let link = document.createElement('link');
+        let attrRel = document.createAttribute('rel');
+        let attrHref = document.createAttribute('href');
 
         attrRel.value = "stylesheet";
         attrHref.value = this.style;
@@ -63,11 +63,11 @@ var bars = {
     }
 };
 
-if (currentMonth > 8 && countFullWeeksBeforeNY % 2 != 0) {
+if (currentMonth > 8 && countFullWeeksBeforeNY % 2 !== 0) {
     bars.swap();
 }
-else if (currentMonth == 8) {
-    if (currentDate >= dateFirstMondayInSeptember && countFullWeeksBeforeNY % 2 != 0) {
+else if (currentMonth === 8) {
+    if (currentDate >= dateFirstMondayInSeptember && countFullWeeksBeforeNY % 2 !== 0) {
         bars.swap();
     }
     else if (currentDate < dateFirstMondayInSeptember) {
@@ -76,12 +76,12 @@ else if (currentMonth == 8) {
 }
 else if (currentMonth < 8) {
     if (currentMonth <= 4 && currentMonth >= 0) {
-        if ((currentMonth == 0) && (currentDate < dateFirstMondayInJanuary)) {
-            if (dayFirstOfSeptember == 0 || dayFirstOfSeptember > 4) {
+        if ((currentMonth === 0) && (currentDate < dateFirstMondayInJanuary)) {
+            if (dayFirstOfSeptember === 0 || dayFirstOfSeptember > 4) {
                 bars.swap();
             }
         }
-        else if (countFullWeeksAfterNY % 2 == 1) { // Fix for 2019 year
+        else if (countFullWeeksAfterNY % 2 === 0) {
             bars.swap();
         }
     }
